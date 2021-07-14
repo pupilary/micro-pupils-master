b03f092619b86d2b4dced2b54a19bf509d85f25b
takesi <nanasetakesi@yahoo.co.jp>
2021-07-14 22:08:52 +0800

    feat: generator new module

---
+++ micro-business/provider-cloud-uas/src/main/java/com/pupils/ProviderCloudUasApplication.java
@@ -0,0 +1,17 @@
+package com.pupils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author takesi
+ * @date 2021-07-14
+ */
+@SpringBootApplication
+public class ProviderCloudUasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderCloudUasApplication.class, args);
+    }
+
+}
